6eb977d4859e1f631f4650d298d3934106c8c5eb
Juan Rodriguez <juan.j.rodriguez2@gmail.com>
2019-08-28 19:55:57 -0500

    started first menu item

--- src/components/views/MenuHome.vue
+++ src/components/views/MenuHome.vue
@@ -3,8 +3,27 @@
     <div class="menu-container" size="A4">
       <div class="menu-header">
         <div class="menu-header-top-border"></div>
+        <div class="menu-header-details"></div>
       </div>
-      <div class="menu-body"></div>
+      <div class="menu-body">
+        <div class="menu-item-card">
+          <div class="menu-item-header">
+            <div class="menu-item-title">
+              <p>Ripe Mango Salad</p>
+              <p>Bankok, Thailand | 14°5'N 90°5667'W</p>
+            </div>
+            <div class="menu-item-details">
+              <span>13</span>
+              <span>From a C cone</span>
+              <span>with your H hands</span>
+              <span>on a S stick</span>
+            </div>
+          </div>
+          <div class="menu-item-body">
+            <p>poached shrimp, shredded coconut, bean sprouts, carrot, red onion, scallion, peanuts, cilantro, basil</p>
+          </div>
+        </div>
+      </div>
       <div class="menu-footer"></div>
     </div>
   </div>
@@ -12,7 +31,7 @@
 
 <script>
 export default {
-  name: 'menu'
+  name: 'menuHome'
 }
 </script>
 
@@ -55,8 +74,28 @@ export default {
 }
 .menu-header-top-border {}
 .menu-body {
-  border: 1px solid blue;
+  /* border: 1px solid blue; */
   flex: 1;
+}
+.menu-item-card {
+  border: 2px solid black;
+  padding: 6px;
+  min-height: 130px;
+  display: flex;
+  flex-direction: column;
+}
+.menu-item-card .menu-item-header, .menu-item-body {
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+}
+.menu-item-card .menu-item-header .menu-item-title {
+  font-size: 1.5em;
+}
+.menu-item-card .menu-item-header .menu-item-title p:nth-of-type(2) {
+  border-top: 1px dotted black;
 }
 .menu-footer {
   border: 1px solid green;
